f1d10930ddd2fac4d9c6fcb706bdb7579768e213
Вини <yvinivitin@gmail.com>
2021-08-11 21:24:02 +0300

    fixed remarks

--- source/js/main.js
+++ source/js/main.js
@@ -15,52 +15,54 @@ function formSubmit(formSelector, nameSelector, telSelector, textSelector, agree
     localStorage.setItem('text', text.value);
     localStorage.setItem('agreement', agreement.value);
 
-    name.value = "";
-    tel.value = "";
-    text.value = "";
+    name.value = '';
+    tel.value = '';
+    text.value = '';
     agreement.checked = false;
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   [].forEach.call(document.querySelectorAll('.ru-number-mask'), function (input) {
     let keyCode;
     function mask(event) {
         event.keyCode && (keyCode = event.keyCode);
         let pos = this.selectionStart;
         if (pos < 3) event.preventDefault();
-        let matrix = "+7 (___) ___-__-__",
+        let matrix = '+7 (___) ___-__-__',
             i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, ""),
+            def = matrix.replace(/\D/g, ''),
+            val = this.value.replace(/\D/g, ''),
             newValue = matrix.replace(/[_\d]/g, function (a) {
                 return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
             });
-        i = newValue.indexOf("_");
+        i = newValue.indexOf('_');
         if (i != -1) {
             i < 5 && (i = 3);
             newValue = newValue.slice(0, i);
         }
         let reg = matrix.substr(0, this.value.length).replace(/_+/g,
             function (a) {
-                return "\\d{1," + a.length + "}";
-            }).replace(/[+()]/g, "\\$&");
-        reg = new RegExp("^" + reg + "$");
+                return '\\d{1,' + a.length + '}';
+            }).replace(/[+()]/g, '\\$&');
+        reg = new RegExp('^' + reg + '$');
         if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = newValue;
-        if (event.type == "blur" && this.value.length < 5) this.value = "";
+        if (event.type == 'blur' && this.value.length < 5) this.value = '';
     }
 
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-    input.addEventListener("keydown", mask, false);
+    input.addEventListener('input', mask, false);
+    input.addEventListener('focus', mask, false);
+    input.addEventListener('blur', mask, false);
+    input.addEventListener('keydown', mask, false);
   });
 
   var popupOpen = document.querySelector('.header__button');
   var popup = document.querySelector('.popup');
   var popupClose = popup.querySelector('.popup__close');
 
-  popupOpen.addEventListener('click', function() {
+  popupOpen.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
     popup.style.display = 'block';
 
     popupClose.addEventListener('click', function() {
@@ -82,6 +84,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
   formSubmit('#questions-form', '#name', '#tel', '#text', '#agreement');
   formSubmit('#popup-form', '#popup-name', '#popup-tel', '#popup-text', '#popup-agreement');
+
+  if (screen.width < 1024) {
+    var cutText = document.querySelector('.about__wrapper p:last-of-type').textContent.substr(0, 200) + '...';
+    document.querySelector('.about__wrapper p:last-of-type').textContent = cutText;
+  }
 
   if (screen.width < 768) {
     var contactsElement = document.querySelector('.footer__contacts-text');
